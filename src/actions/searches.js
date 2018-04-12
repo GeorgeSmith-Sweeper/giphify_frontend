238fe1aa2ad4f05d1bd5f6a4a851eb0d2703d841
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-04-11 23:48:25 -0400

    add handleAddSearch to deal with post requests in form

--- src/actions/searches.js
+++ src/actions/searches.js
@@ -4,3 +4,24 @@ export const addSearchQuery = query => ({
   type: ADD_SEARCH_QUERY,
   query
 });
+
+export const handleAddSearchQuery = (userData) => {
+  return dispatch =>
+    fetch('/api/searches', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: userData.query,
+      }),
+    })
+      .then((res) => {
+        if (res.status >= 200 && res.status < 300) {
+          console.log(res);
+          dispatch(addSearchQuery(userData.query));
+        }
+      })
+      .catch(error => console.log(error));
+};
