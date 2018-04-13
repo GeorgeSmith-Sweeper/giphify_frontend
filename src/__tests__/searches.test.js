ee43b2376789bc390ecce515373569666240b0c9
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-04-13 11:10:32 -0400

    add reducer test for a query

--- src/__tests__/searches.test.js
+++ src/__tests__/searches.test.js
@@ -1,4 +1,5 @@
 import { ADD_SEARCH_QUERY, addSearchQuery } from '../actions/searches';
+import reducer from '../reducers/searches';
 
 describe('actions', () => {
   it('should create an action to addSearchQuery ', () => {
@@ -8,5 +9,21 @@ describe('actions', () => {
       query
     };
     expect(addSearchQuery(query)).toEqual(expectedAction);
+  });
+
+  describe('searches reducer', () => {
+    it('should return the initial state', () => {
+      expect(reducer(undefined, {})).toEqual({});
+    });
+
+    it('should handle ADD_SEARCH_QUERY', () => {
+      const query = 'Keyboard cat';
+      const action = {
+        type: ADD_SEARCH_QUERY,
+        query
+      };
+
+      expect(reducer({ query: '' }, action)).toEqual({ query: 'Keyboard cat' });
+    });
   });
 });
